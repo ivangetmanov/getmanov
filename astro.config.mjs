b9b2af1c9f737d3f062c8e8ac9fdb47e9c8a798f
ivangetmanov <114616653+ivangetmanov@users.noreply.github.com>
2025-12-25 16:16:15 +0700

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://getmanov.com"
+  site: "https://getmanov.com",
+  integrations: [sitemap()],
 });
